76651b721a3bf308c55bd29b95eaa077ff3413db
Tamara Atanasoska <atanasoska.tamara@gmail.com>
2023-01-06 22:54:47 +0100

    Add adapted fine-tuning code(needs GPU testing)

---
+++ conclusion_generation/fine_tuning/dataloader.py
@@ -0,0 +1,51 @@
+import torch
+
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    def __init__(self, dataframe, tokenizer, source_len, summ_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.source_len = source_len
+        self.summ_len = summ_len
+        self.claim = self.data.Claim
+        self.premises = self.data.Premises
+
+    def __len__(self):
+        return len(self.premises)
+
+    def __getitem__(self, index):
+        claim = str(self.claim[index])
+        claim = " ".join(claim.split())
+
+        premises = str(self.premises[index])
+        premises = premises.replace(" ### ", " ")
+        premises = " ".join(premises.split())
+
+        source = self.tokenizer.batch_encode_plus(
+            [claim],
+            max_length=self.source_len,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+        target = self.tokenizer.batch_encode_plus(
+            [premises],
+            max_length=self.summ_len,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        source_ids = source["input_ids"].squeeze()
+        source_mask = source["attention_mask"].squeeze()
+        target_ids = target["input_ids"].squeeze()
+        target_mask = target["attention_mask"].squeeze()
+
+        return {
+            "source_ids": source_ids.to(dtype=torch.long),
+            "source_mask": source_mask.to(dtype=torch.long),
+            "target_ids": target_ids.to(dtype=torch.long),
+            "target_ids_y": target_ids.to(dtype=torch.long),
+        }
---
+++ conclusion_generation/fine_tuning/fine-tune.py
@@ -0,0 +1,171 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import wandb
+import yaml
+
+from dataloader import CustomDataset
+from torch import cuda
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.optim.lr_scheduler import ExponentialLR
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+device = "cuda" if cuda.is_available() else "cpu"
+
+
+def train(epoch, tokenizer, model, device, loader, optimizer, scheduler):
+    model.train()
+    for _, data in enumerate(loader, 0):
+        y = data["target_ids"].to(device, dtype=torch.long)
+        y_ids = y[:, :-1].contiguous()
+        lm_labels = y[:, 1:].clone().detach()
+        lm_labels[y[:, 1:] == tokenizer.pad_token_id] = -100
+        ids = data["source_ids"].to(device, dtype=torch.long)
+        mask = data["source_mask"].to(device, dtype=torch.long)
+
+        outputs = model(
+            input_ids=ids,
+            attention_mask=mask,
+            decoder_input_ids=y_ids,
+            labels=lm_labels,
+        )
+        loss = outputs[0]
+
+        if _ % 10 == 0:
+            wandb.log({"Training Loss": loss.item()})
+
+        if _ % 500 == 0:
+            print(f"Epoch: {epoch}, Loss:  {loss.item()}")
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+
+def validate(epoch, tokenizer, model, device, loader):
+    model.eval()
+    predictions = []
+    actuals = []
+    with torch.no_grad():
+        for _, data in enumerate(loader, 0):
+            y = data["target_ids"].to(device, dtype=torch.long)
+            ids = data["source_ids"].to(device, dtype=torch.long)
+            mask = data["source_mask"].to(device, dtype=torch.long)
+
+            generated_ids = model.generate(
+                input_ids=ids,
+                attention_mask=mask,
+                max_length=25,
+                min_length=4,
+                do_sample=True,
+                num_beams=4,
+                top_k=20,
+                temperature=0.75,
+                no_repeat_ngram_size=2,
+                repetition_penalty=1.25,
+            )
+
+            preds = [
+                tokenizer.decode(
+                    g, skip_special_tokens=True, clean_up_tokenization_spaces=True
+                )
+                for g in generated_ids
+            ]
+            target = [
+                tokenizer.decode(
+                    t, skip_special_tokens=True, clean_up_tokenization_spaces=True
+                )
+                for t in y
+            ]
+            if _ % 100 == 0:
+                print(f"Completed {_}")
+
+            predictions.extend(preds)
+            actuals.extend(target)
+    return predictions, actuals
+
+
+def main(config):
+    # wandb.init(project="", entity="")
+
+    torch.manual_seed(*config["seed"].values())
+    np.random.seed(*config["seed"].values())
+    torch.backends.cudnn.deterministic = True
+
+    tokenizer = T5Tokenizer.from_pretrained("t5-base", model_max_length=512)
+
+    df = pd.read_csv("premises_claims.csv", encoding="latin-1")
+    df = df[["Premises", "Claim"]]
+    df.Premises = "summarize: " + df.Premises
+    print(df.head())
+
+    train_size = 0.8
+    train_dataset = df.sample(frac=train_size, random_state=config["seed"]["value"])
+    val_dataset = df.drop(train_dataset.index).reset_index(drop=True)
+    train_dataset = train_dataset.reset_index(drop=True)
+
+    print("FULL Dataset: {}".format(df.shape))
+    print("TRAIN Dataset: {}".format(train_dataset.shape))
+    print("TEST Dataset: {}".format(val_dataset.shape))
+
+    training_set = CustomDataset(
+        train_dataset,
+        tokenizer,
+        *config["max_len"].values(),
+        *config["conclusion_len"].values(),
+    )
+    val_set = CustomDataset(
+        val_dataset,
+        tokenizer,
+        *config["max_len"].values(),
+        *config["conclusion_len"].values(),
+    )
+
+    train_params = {
+        "batch_size": config["train_batch_size"]["value"],
+        "shuffle": True,
+        "num_workers": 0,
+    }
+
+    val_params = {
+        "batch_size": config["valid_batch_size"]["value"],
+        "shuffle": False,
+        "num_workers": 0,
+    }
+
+    training_loader = DataLoader(training_set, **train_params)
+    val_loader = DataLoader(val_set, **val_params)
+
+    model = T5ForConditionalGeneration.from_pretrained("t5-base")
+    model = model.to(device)
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=config["learning_rate"]["value"]
+    )
+    scheduler = ExponentialLR(optimizer, gamma=config["gamma"]["value"])
+
+    wandb.watch(model, log="all")
+    print("Initiating Fine-Tuning for the model on our dataset")
+
+    for epoch in range(*config["train_epochs"].values()):
+        train(epoch, tokenizer, model, device, training_loader, optimizer, scheduler)
+
+    print(
+        "Now generating consclusions on our fine tuned model for the validation dataset and saving it in a dataframe"
+    )
+    for epoch in range(*config["val_epochs"].values()):
+        predictions, actuals = validate(epoch, tokenizer, model, device, val_loader)
+        final_df = pd.DataFrame({"Conclusion": predictions, "Premises": actuals})
+        final_df.to_csv("conclusions.csv")
+        print("Generated conclusions")
+
+
+if __name__ == "__main__":
+
+    with open("config-defaults.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    main(config)
