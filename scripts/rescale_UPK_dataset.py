dbbb80f7f1a38b3819725e00f62cedd3ef021f57
Galina Ryazanskaya <galina.ryazanskaya@jetbrains.com>
2023-01-11 15:21:38 +0100

    add script to rescale UPK_dataset

---
+++ scripts/rescale_UPK_dataset.py
@@ -0,0 +1,27 @@
+import argparse
+from pathlib import Path
+
+import pandas as pd
+
+
+def read_UPK_data(path, sep='\t'):
+    df = pd.read_csv(path, sep=sep)
+    df = df[['topic', 'sentence_1', 'sentence_2', 'label']]
+    df['regression_label_binary'] = df['label'].apply(lambda x: 1 if x in ['HS', 'SS'] else 0)
+    return df[['topic', 'sentence_1', 'sentence_2', 'regression_label_binary']]
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--upk_path', type=lambda x: Path(x))
+    arg_parser.add_argument('--out_file', type=lambda x: Path(x))
+    arg_parser.add_argument('--input_type', type=str, default='tsv')  # tsv or csv
+    args = arg_parser.parse_args()
+
+    sep = '\t' if args.input_type == 'tsv' else ','
+    df = read_UPK_data(args.upk_path, sep=sep)
+    df.to_csv(args.out_file, index=False)
+
+
+if __name__ == "__main__":
+    main()
